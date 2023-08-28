820e1a0b5c19cb844a898ee1d480c3ba086e173f
Luciana Vivarelli <lucianavivarelli@hotmail.com>
2023-08-28 19:03:35 -0300

    new carroussel starting

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { NavBar } from "../components/NavBar/NavBar";
-import { Footer } from "../components/footer/footer";
+import { Footer } from "../components/Footer/Footer";
 import { Banner } from "../components/Banner/Banner";
 import { Services } from "../components/ServiceHN/Services";
 import { Contact } from "../components/Contact/Contact";
--- src/components/TechAndInfo/Tech.jsx
+++ src/components/TechAndInfo/Tech.jsx
@@ -1,11 +1,31 @@
 // components/Tech.jsx
-
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
  export const Tech = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2
+    }
+  };
+
   const technologies = [
     { name: 'Python', icon: 'python' },
     { name: 'Django', icon: 'django' },
