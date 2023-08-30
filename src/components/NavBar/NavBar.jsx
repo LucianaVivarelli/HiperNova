2916371c55c7173c9136190e4d70f832126a209c
AndersonGuassu <andersonguassu@hotmail.com>
2023-08-30 12:18:09 -0300

    Arrumando importações

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/logo.jpeg'
-import { SlSocialLinkedin, SlSocialTwitter, SlSocialInstagram, SlSocialFacebook } from 'react-icons/Sl';
+import { SlSocialLinkedin, SlSocialTwitter, SlSocialInstagram, SlSocialFacebook } from 'react-icons/sl';
 import { useState, useEffect } from 'react'
 
 import {
