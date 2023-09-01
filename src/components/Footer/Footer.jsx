9425f31ca51ea4c92c7fbb3c94eac34b3ddde75e
Luciana Vivarelli <lucianavivarelli@hotmail.com>
2023-08-31 22:23:15 -0300

    responsive finish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,15 @@
 
-import { Container, Row, Col } from 'react-bootstrap';
-import {  FaInstagram, FaTwitter } from 'react-icons/fa';
-
+import copy from '../../assets/copy.png'
 
 
 export const Footer = () => {
   return (
     <div className="footer ">
-      <Container>
-        <Row>
-          <Col md={4}>
-          <div className="social-icon d-inline-flex">
-              <a href="https://www.instagram.com/hipernovasol/">
-                <FaInstagram />
-              </a>
-              <a href="https://twitter.com/hipernovasol">
-                <FaTwitter />
-              </a>
+     
+          <div className=" ">
+              <img src={copy} className='copy' alt="copyright" />
             </div>
-          </Col>
-        
-        </Row>
-      </Container>
+          
     </div>
   );
 };
